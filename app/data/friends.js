dcd07a575243aff2a389c7b4d051ba658ed4698e
smcnaught <stephaniemcnaught@gmail.com>
2017-09-13 22:11:11 -0600

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,12 @@
+// You should save your application's data inside of app/data/friends.js as an array of objects. 
+// Each of these objects should roughly follow the format below.
+
+// {
+//     "name": "Ahmed",
+//     "photo": "https://////.....",
+//     "scores": [
+//         5,
+//         1,
+//         ...
+//     ]
+// }
