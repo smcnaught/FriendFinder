347f1a079390ace741988c1446632b9cb3f13076
smcnaught <stephaniemcnaught@gmail.com>
2017-09-23 09:03:12 -0600

    final files

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,6 +15,86 @@ var friends = [
             4,
             1
         ]
-    }
+    },
+    {
+        "name": "Mr. Coffee",
+        "photo": "https://cdn.pixabay.com/photo/2017/09/04/18/39/coffee-2714970_1280.jpg",
+        "scores":[
+            3,
+            2,
+            3,
+            4,
+            5,
+            2,
+            2,
+            2,
+            4,
+            3
+        ]
+    },
+    {
+        "name": "Ms. Whiskers",
+        "photo": "https://cdn.pixabay.com/photo/2017/08/23/08/33/cats-eyes-2671903_1280.jpg",
+        "scores":[
+            5,
+            4,
+            5,
+            5,
+            4,
+            5,
+            4,
+            5,
+            4,
+            5
+        ]
+    },
+    {
+        "name": "Ballin Eagle",
+        "photo": "https://cdn.pixabay.com/photo/2017/09/04/20/52/bald-eagle-2715461_1280.jpg",
+        "scores":[
+            2,
+            3,
+            1,
+            4,
+            3,
+            2,
+            2,
+            4,
+            2,
+            5
+        ]
+    },
+    {
+        "name": "UR Scary",
+        "photo": "https://cdn.pixabay.com/photo/2017/08/23/08/01/animals-2671825_1280.jpg",
+        "scores":[
+            4,
+            5,
+            4,
+            4,
+            1,
+            4,
+            4,
+            3,
+            4,
+            4
+        ]
+    },
+    {
+        "name": "True Love",
+        "photo": "https://cdn.pixabay.com/photo/2013/10/16/23/08/elephants-196613_1280.jpg",
+        "scores":[
+            3,
+            3,
+            5,
+            3,
+            2,
+            3,
+            3,
+            5,
+            3,
+            1
+        ]
+    },
 ]
 module.exports = friends;
